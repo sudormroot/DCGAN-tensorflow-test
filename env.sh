11f2ef7ee7c7f4d65d7b52a69e2173873377231a
sudormroot <sudormroot@163.com>
2020-05-30 09:44:39 +0800

    add

--- env.sh
+++ env.sh
@@ -1,1 +1,3 @@
-export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:$LD_LIBRARY_PATH:/usr/local/cuda/extras/CUPTI/lib64
+#export LD_LIBRARY_PATH=/usr/local/cuda/lib64/
+export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:$LD_LIBRARY_PATH:/usr/local/cuda/extras/CUPTI/lib64:/usr/local/cuda/lib64/
+
